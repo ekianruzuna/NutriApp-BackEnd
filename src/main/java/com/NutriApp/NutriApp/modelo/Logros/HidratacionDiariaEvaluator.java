84812191a89ad0ef09697dab233610cb16347a6a
ekian <ekianuruzuna@gmail.com>
2026-08-24 14:11:17 -0300

    Arreglo un error que tenia un bucle cuando spring intentaba iniciaizar los beans

--- src/main/java/com/NutriApp/NutriApp/modelo/Logros/HidratacionDiariaEvaluator.java
+++ src/main/java/com/NutriApp/NutriApp/modelo/Logros/HidratacionDiariaEvaluator.java
@@ -1,25 +1,36 @@
 package com.NutriApp.NutriApp.modelo.Logros;
 
+import com.NutriApp.NutriApp.modelo.Dia;
+import com.NutriApp.NutriApp.modelo.Hidratacion;
 import com.NutriApp.NutriApp.modelo.Usuario;
 import com.NutriApp.NutriApp.modelo.enums.TipoLogro;
+import com.NutriApp.NutriApp.repository.HidratacionRepository;
 import com.NutriApp.NutriApp.service.DiaService;
 import com.NutriApp.NutriApp.service.HidratacionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class HidratacionDiariaEvaluator implements LogroEvaluator{
 
-    private final HidratacionService hidratacionService;
+    private final DiaService diaService;
+    private final HidratacionRepository hidratacionRepository;
 
     @Override
     public boolean seCumple(LocalDate date, Usuario user) {
-        int totalAguaConsumida = hidratacionService.obtenerTotalAguaPorFecha(date);
+        Dia dia = diaService.obtenerODiaOCrear(date);
 
-        return (totalAguaConsumida >= 2000);
+        Optional<Hidratacion> totalAguaConsumida = hidratacionRepository.findByDia(dia);
+
+        if (totalAguaConsumida.isEmpty()){
+            return false;
+        }
+
+        return (totalAguaConsumida.get().getCantidadMl() >= 2000);
     }
 
     @Override
